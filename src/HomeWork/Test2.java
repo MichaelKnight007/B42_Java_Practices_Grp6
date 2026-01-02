56405bf1982b309ad57b5f04d65f72cf9e4afe5c
rajae.ha <rajae.hamida@gmail.com>
2026-01-02 15:37:07 -0500

    Class 2 Created By Rajae

--- src/HomeWork/Test2.java
+++ src/HomeWork/Test2.java
@@ -3,5 +3,8 @@ package HomeWork;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Class 2 is created!");
+
+        System.out.println(" Class Created By Rajae");
+
     }
 }
